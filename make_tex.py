2bb32f49702f3de35add34e5cd895f3b0cb84c57
Achyut Reddy <achyutreddy24@gmail.com>
2015-03-28 22:55:31 -0400

    Add tex strings to py file

---
+++ make_tex.py
@@ -0,0 +1,28 @@
+preambleTEX = """\documentclass[addpoints]{{exa}m}"""
+
+documentTEX = """\begin{{document}}
+
+\makebox[\textwidth]{{Name and section:\enspace\hrulefill}}
+\vspace{{0.2in}}
+\makebox[\textwidth]{{Instructor’s name:\enspace\hrulefill}}
+
+
+\begin{{questions}}
+{MQuestions}
+\end{{questions}}
+
+{OQuestion}
+
+\end{{document}}
+
+
+"""
+
+MQuestionTEX = """\question
+{Question}
+\begin{{choices}}
+{Choices}
+\end{{choices}}
+"""
+
+ChoiceTEX = """\choice {choice}"""
